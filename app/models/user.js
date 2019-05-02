8122937942b20fcdeb56b4d6d8bc54ef8f9a3c05
panch2203 <panch2203@gmail.com>
2019-05-02 07:40:57 -0500

    agregue usuarios en el select

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 const { Model, attr  } = DS;
 
 export default class UserModel extends Model {
-  @attr("string")   code;
-	@attr("string")   status;
-	@attr("string")   total_users;
+  @attr("string")   username;
+	@attr("string")   email;	
 }
--- app/routes/application.js
+++ app/routes/application.js
@@ -4,7 +4,8 @@ import { hash } from 'rsvp';
 export default class ApplicationRoute extends Route {
 	model(){
 		return hash({
-					games: this.store.findAll('game'),					
+					games: this.store.findAll('game'),
+					users: this.store.findAll('user')				
 		})
 	}
 }
