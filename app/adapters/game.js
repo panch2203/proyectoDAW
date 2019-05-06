82164c2535d7a93d563fc6f02309a4e8ba858338
Gerhard1902 <g.rardo1920@gmail.com>
2019-05-05 23:02:12 -0500

    ya se empieza el juego y modifico visibilidad de botones

--- app/adapters/game.js
+++ app/adapters/game.js
@@ -2,6 +2,6 @@ import ApplicationAdapter from './application';
 
 export default class GameAdapter extends ApplicationAdapter {
 	urlForUpdateRecord(id,modelName,snapshot){
-		return `https://polar-basin-61192.herokuapp.com/games/:$(id)/start`;
+		return `https://polar-basin-61192.herokuapp.com/games/${id}/start`;
 	}
 }
